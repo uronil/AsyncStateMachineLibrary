69245d62d3f14165f4e4eb56f103c12cba2b5ef9
Ilya Ronzhin <qvepoy@gmail.com>
2023-03-06 01:23:17 +0300

    Add nuspec.

--- AsyncStateMachineLibrary/Properties/AssemblyInfo.cs
+++ AsyncStateMachineLibrary/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AsyncStateMachineLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Async state machine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ilya-ronzhin")]
 [assembly: AssemblyProduct("AsyncStateMachineLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
